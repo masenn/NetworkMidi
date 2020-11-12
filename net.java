89a89426e67e3a3e7a2565b0267ff8c5e637b012
masenn <74339498+masenn@users.noreply.github.com>
2020-11-11 22:23:27 -0600

    Create net.java

---
+++ net.java
@@ -0,0 +1,58 @@
+package midi;
+
+import java.io.*;
+import java.util.*;
+import java.net.*;        
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.ShortMessage;
+import static javax.sound.midi.ShortMessage.*;
+
+public class net {
+    public static boolean clientConnected = false;
+    public static boolean hostConnected = false;
+    public static Socket connectHost(int port) throws IOException{
+        ServerSocket ss = new ServerSocket(port);
+        Socket wait = ss.accept();
+        hostConnected=true;
+        System.out.println("Host Connected");
+        return wait;
+    }
+    public static Socket connectClient(String ip, int port) throws IOException{
+        Socket s = new Socket(ip,port);
+        clientConnected=true;
+        System.out.println("Client Connect");
+        return s;
+    }
+    static void write(String e, Socket socket) throws IOException{
+        DataOutputStream out;
+        out = new DataOutputStream(socket.getOutputStream());
+        out.writeUTF(e);
+        out.flush();
+    }
+    //to recieve serial messages from the socket
+    static String getMessage(Socket s, Deque deque) throws IOException{
+        DataInputStream in = new DataInputStream(s.getInputStream());
+        String e = in.readUTF();
+        deque.add(e);
+        return e;
+    }
+    public static ShortMessage pullApart(String cmd) throws InvalidMidiDataException{
+        int velocity = 0;
+        String[] out = cmd.split("-");
+        String command = out[0]; int icommand = Integer.parseInt(command);
+        System.out.println("Command: "+command);
+        String channel = out[1];int ichannel = Integer.parseInt(channel);
+        System.out.println("Channel: "+channel);
+        String data1 = out[2]; int idata1 = Integer.parseInt(data1);
+        System.out.println("Data 1: "+data1);
+        String data2 = out[3]; int idata2 = Integer.parseInt(data2);
+        if (idata2==64){
+            icommand=NOTE_ON;
+        } if(idata2==0){
+            icommand=NOTE_OFF;
+        }
+        System.out.println("Data 2: "+data2);
+        ShortMessage mes = new ShortMessage(icommand, ichannel, idata1, idata2);
+        return mes;
+    }
+}
